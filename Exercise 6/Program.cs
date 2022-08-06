67e2c6a082811fe126e03db24f8fd8eb115a7bbf
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-07 01:16:54 +0300

    Add Exercise 6

---
+++ Exercise 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+        Console.WriteLine("нет");
+}
+
