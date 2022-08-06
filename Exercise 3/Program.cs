ef29a86e8fe482557fc445db983f36c9367d16d5
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-07 00:55:48 +0300

    Add Exercise 3

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер: ");
+int N = int.Parse(Console.ReadLine());
+if (N == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (N == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (N == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (N == 4 )
+{
+    Console.WriteLine("Четверг");
+}
+else if (N == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (N == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (N == 7)
+{
+    Console.WriteLine("Воскресение");
+}
+
+
