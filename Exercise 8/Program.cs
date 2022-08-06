ff7efa781d45aada915140e8c961cd6cc52b5a26
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-07 01:17:36 +0300

    Add Exercise 8

---
+++ Exercise 8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for(int i=2; i<=number; i++) 
+{
+   if(i%2 == 0) 
+   {
+    Console.Write("{0} ", i);
+   }
+}
