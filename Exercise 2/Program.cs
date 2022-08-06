dd2c2de53e525cb105256e737c4062b96a4cda3b
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-07 00:53:37 +0300

    Add Exercise 2

---
+++ Exercise 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int NumberB = int.Parse(Console.ReadLine());
+if (NumberA > NumberB)
+{
+    Console.WriteLine($"max = {NumberA}");
+}
+else
+{
+        Console.WriteLine($"max = {NumberB}");
+}
